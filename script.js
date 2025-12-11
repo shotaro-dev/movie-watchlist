2d34ca262fae5ebb8d5741132289d7f814738ed4
shotaro-dev <231440159+shotaro-dev@users.noreply.github.com>
2025-12-11 14:27:53 +0900

    Feat: Add movie search functionality via searchForm

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const resultsEl = document.getElementById("results-container");
 const watchlistEl = document.getElementById("watchlist-container");
+const searchInputEl = document.getElementById("search-input");
+const formEl = document.getElementById("search-form");
+
+
+
 
 const API = "1b1e0652";
-const TITLE = "terminator";
+let title = "terminator";
 const baseUrl = "https://www.omdbapi.com/";
-const queryParams = `?apikey=${API}&s=${TITLE}`;
-const url = baseUrl + queryParams;
+let queryParams = `?apikey=${API}&s=${title}`;
+let url = baseUrl + queryParams;
 
 const plusIcon = `<svg class="w-full h-full" viewBox="0 0 100 100" fill="black"  xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M50 75C63.8071 75 75 63.8071 75 50C75 36.1929 63.8071 25 50 25C36.1929 25 25 36.1929 25 50C25 63.8071 36.1929 75 50 75ZM53.125 40.625C53.125 38.8991 51.7259 37.5 50 37.5C48.2741 37.5 46.875 38.8991 46.875 40.625V46.875H40.625C38.8991 46.875 37.5 48.2741 37.5 50C37.5 51.7259 38.8991 53.125 40.625 53.125H46.875V59.375C46.875 61.1009 48.2741 62.5 50 62.5C51.7259 62.5 53.125 61.1009 53.125 59.375V53.125H59.375C61.1009 53.125 62.5 51.7259 62.5 50C62.5 48.2741 61.1009 46.875 59.375 46.875H53.125V40.625Z" />
@@ -19,13 +24,40 @@ const searchIcon = `
 <path fill-rule="evenodd" clip-rule="evenodd" d="M43.75 31.25C36.8464 31.25 31.25 36.8464 31.25 43.75C31.25 50.6536 36.8464 56.25 43.75 56.25C50.6536 56.25 56.25 50.6536 56.25 43.75C56.25 36.8464 50.6536 31.25 43.75 31.25ZM25 43.75C25 33.3947 33.3947 25 43.75 25C54.1053 25 62.5 33.3947 62.5 43.75C62.5 47.7995 61.2163 51.5491 59.0336 54.6142L74.0847 69.6653C75.3051 70.8857 75.3051 72.8643 74.0847 74.0847C72.8643 75.3051 70.8857 75.3051 69.6653 74.0847L54.6142 59.0336C51.5491 61.2163 47.7995 62.5 43.75 62.5C33.3947 62.5 25 54.1053 25 43.75Z" />
 </svg>`;
 
+
+
 let watchlist = [];
 
+
+if(searchInputEl){
+    searchInputEl.addEventListener("input", (e) => {
+        title = e.target.value.trim();
+        console.log("Search term:", title);
+        // 他の変数も更新しないと変わらない
+        queryParams = `?apikey=${API}&s=${title}`;
+        url = baseUrl + queryParams;
+        console.log("Updated URL:", url);
+
+    });
+}
+
+if(formEl){
+    formEl.addEventListener("submit", (e) => {
+        e.preventDefault();
+        console.log("Form submitted. Searching for:", title);
+        // Clear previous results
+        fetchMovies();
+    });
+}
+
 async function fetchMovies() {
+    console.log("fetchMovies called with title:", title);
+    console.log(url)
+
   const searchResponse = await fetch(url);
   const searchData = await searchResponse.json();
 
-  console.log(searchData);
+//   console.log(searchData);
   let movies;
   // promise.allで複数のfetchを全て通るか判定
   try {
@@ -48,11 +80,14 @@ async function fetchMovies() {
 
   //   console.log(movies);
   //   if (!movies) return;
-  console.log(movies);
+//   console.log(movies);
+
+  resultsEl.innerHTML = "";
+
   movies.forEach((movie, index) => {
     const { Title, Genre, Plot, imdbRating, Runtime, imdbID } = movie;
     const Poster = searchData.Search[index].Poster;
-    console.log(Title, Genre, Plot, imdbRating, Runtime);
+    // console.log(Title, Genre, Plot, imdbRating, Runtime);
     //数字だけfont-monoにするため分離
     const [num, unit] = Runtime.split(" ");
     const movieObj = {
@@ -95,7 +130,7 @@ async function fetchMovies() {
   const addToWatchlistBtns = 
     document.querySelectorAll(".addWatchlist-btn")
   
-  console.log(addToWatchlistBtns);
+//   console.log(addToWatchlistBtns);
   if (addToWatchlistBtns) {
     addToWatchlistBtns.forEach((btn) => {
       btn.addEventListener("click", () => {
@@ -114,6 +149,7 @@ async function fetchMovies() {
   }
 }
 
+// 表示してるpageにない要素はnullになる。
 if(resultsEl){
     fetchMovies();
 } else {
